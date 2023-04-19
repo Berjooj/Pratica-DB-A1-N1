74cec3982e5f705d42116984f9b0f0f886a63596
Israel Andreotti <95102493+israelitas@users.noreply.github.com>
2023-04-19 13:35:36 -0300

    Delete tipo_servico.sql

--- tipo_servico.sql
+++
@@ -1,6 +0,0 @@
-insert into tipo_servico (id, nome, preco, descricao) values (1, 'Lavanderia', '30.00', 'Sua roupa lavada com o melhores produtos do mercado e entregue na porta de seu quarto');
-insert into tipo_servico (id, nome, preco, descricao) values (2, 'Academia', '40.00', 'Fiwue em forma, mesmo nas férias com a nossa academia');
-insert into tipo_servico (id, nome, preco, descricao) values (3, 'Sapateiro', '10.00', 'Seu sapato novinha em folha, pronto para uso');
-insert into tipo_servico (id, nome, preco, descricao) values (4, 'Aluguel de bicicleta', '15.00', 'Conheça os locais mais charmosos do nosso bairro alugando uma bicicleta');
-insert into tipo_servico (id, nome, preco, descricao) values (5, 'Wifi', '8.00', 'Wifi para você se conectar com o mundo');
-
