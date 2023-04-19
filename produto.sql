677b44efa5cebf6382baff6204a8605779c7b5c9
Israel Andreotti <95102493+israelitas@users.noreply.github.com>
2023-04-19 13:36:03 -0300

    Tabelas prontas

---
+++ produto.sql
@@ -0,0 +1,9 @@
+insert into produto (id, id_local_hospedagem, nome, descricao, preco, quantidade_em_estoque) values (1, 1, 'Água mineral s/ gás, 500ml', 'Água mineral Voss 500ml sem gás, vem em garrafa de vidro', '5.00', 1);
+insert into produto (id, id_local_hospedagem, nome, descricao, preco, quantidade_em_estoque) values (2, 2, 'Água mineral c/ gás, 500ml', 'Água mineral Voss 500ml com gás, vem em garrafa de vidro', '5.00', 2);
+insert into produto (id, id_local_hospedagem, nome, descricao, preco, quantidade_em_estoque) values (3, 3, 'Sanduíche de frango', 'Sanduíche de frango, queijo cheddar e nosso molho especial', '20.00', 3);
+insert into produto (id, id_local_hospedagem, nome, descricao, preco, quantidade_em_estoque) values (4, 4, 'Sanduíche Veggie', 'Sanduíche para aqueles que escolheram um estilo de vida saudável: Alface, tomate, rúcula, maionese e cenoura;', '25.00', 4);
+insert into produto (id, id_local_hospedagem, nome, descricao, preco, quantidade_em_estoque) values (5, 5, 'Salgadinho 100g', 'Salgadinho sabor queijo', '8.00', 5);
+insert into produto (id, id_local_hospedagem, nome, descricao, preco, quantidade_em_estoque) values (6, 6, 'Refrigerante', 'Bebida gaseficada', '5.00', 6);
+insert into produto (id, id_local_hospedagem, nome, descricao, preco, quantidade_em_estoque) values (7, 7, 'Batata frita', 'Porção de fritas: 250g das mais deliciosas batatas fritas que você já viu. Acompanha molho especial', '30.00', 7);
+
+
