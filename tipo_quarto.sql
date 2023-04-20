0b58301c54056ea40e46fcce1deec39389d69971
Israel Andreotti <95102493+israelitas@users.noreply.github.com>
2023-04-20 11:50:27 -0300

    Delete tipo_quarto.sql

--- tipo_quarto.sql
+++
@@ -1,7 +0,0 @@
-insert into tipo_quarto (id, nome, preco, descricao) values (1, 'Família', '$643.25', 'Famíliar grande? Sem problemas, venha ficar em um de nossos quartos tamanho família');
-insert into tipo_quarto (id, nome, preco, descricao) values (2, 'Super-Luxo', '$364.46', 'Quarto que oferece o melhor da nossa experiência');
-insert into tipo_quarto (id, nome, preco, descricao) values (3, 'Luxo', '200.00', 'Quarto que oferece uma das melhores experiências possíveis na nossa rede de hospedagem');
-insert into tipo_quarto (id, nome, preco, descricao) values (4, 'Standard', '$389.28', 'Quarto regular');
-insert into tipo_quarto (id, nome, preco, descricao) values (5, 'Economico', '$125.07', 'Apertado de grana mas precisa viajar? Essa opção é para você');
-
-
